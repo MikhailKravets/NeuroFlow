eb8d3a792b46bdbff9e758d09f3e5e72266c1c09
Mikhail Kravets <22kravets22@mail.ru>
2017-11-10 12:04:54 +0200

    Think about data model for module

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,3 +1,13 @@
 /// The future place for code that works with data.
 /// TODO: write functions that loads csv data.
 /// TODO: write functions that write existing NN to file.
+
+
+struct DataSet{
+
+}
+
+
+trait NNData {
+
+}
