ada77d64a6395ff62ce195865a8830026d31af20
Mikhail Kravets <22kravets22@mail.ru>
2017-11-12 19:58:47 +0200

    Turn prototype back

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -2,18 +2,18 @@
 /// The module should contain functions and traits for saving
 /// and restoring of neural networks from files, and buffers
 
-use std::io::{Read, Write};
+
 
 /// obj: it should be neural network to save.
 /// I guess it should be generic type with necessary
 /// implementation of some trait
-fn save<T>(obj: None, &buf: T) where T: Write{
+fn save(obj: None, file_path: &str){
 
 }
 
 /// Function that load NN from file.
 /// Is it possible to return generic type!?
-fn load<T>(&buf: T) -> None where T: Read{
+fn load<T>(file_path: &str) -> None{
 
 }
 
