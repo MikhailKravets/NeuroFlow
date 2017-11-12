77e9159d3e5a2f552ce86d83b3bc7c85f4f11b87
Mikhail Kravets <22kravets22@mail.ru>
2017-11-12 17:03:06 +0200

    Write important functions prototypes

---
+++ src/io/mod.rs
@@ -0,0 +1,28 @@
+/// # IO NeuroFlow module
+/// The module should contain functions and traits for saving
+/// and restoring of neural networks from files, and buffers
+
+
+/// obj: it should be neural network to save.
+/// I guess it should be generic type with necessary
+/// implementation of some trait
+fn save(obj: None, path_file: &str){
+
+}
+
+/// Function that load NN from file.
+/// Is it possible to return generic type!?
+fn load(path_file: &str) -> None{
+
+}
+
+/// Future function for saving in JSON string.
+/// return: JSON string
+fn to_json(obj: None) -> &str {
+
+}
+
+/// Function for deserializing of JSON to NN struct
+fn from_json(s: &str){
+
+}
