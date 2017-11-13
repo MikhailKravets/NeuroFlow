f0b92d06febbef2fd2c2cb9fcc9012d5cae76f4d
Mikhail Kravets <22kravets22@mail.ru>
2017-11-13 19:52:21 +0200

    Document io module

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,6 +1,20 @@
-/// # IO NeuroFlow module
-/// The module should contain functions and traits for saving
-/// and restoring of neural networks from files, and buffers
+//! The module contains functions, structs, enums, and traits
+//! for input/output neural networks. E. g. it can save network
+//! to the file and then loads it back.
+//!
+//! # Example
+//! Saving of neural network:
+//! ```
+//! let mut nn = FeedForward::new(&[2, 2, 1]);
+//! /* train here your neural network */
+//! save(&nn, "test.flow");
+//! ```
+//!
+//! Restoring of neural network:
+//! ```
+//! let mut new_nn: FeedForward = load("test.nn")
+//!     .unwrap_or(FeedForward::new(&[2, 2, 1]));
+//! ```
 
 use std;
 use std::fs::File;
@@ -9,13 +23,25 @@ use serde;
 use bincode;
 use bincode::{serialize, deserialize_from, Infinite};
 
+/// Custom Error enum for handling multiple error types
 #[derive(Debug)]
 pub enum IOError{
     IO(std::io::Error),
     Encoding(bincode::Error)
 }
 
-
+/// Saves given neural network to file specified by `file_path`.
+///
+/// * `obj: &T` - link on neural network;
+/// * `file_path: &str` - path to the file.
+/// * `return -> Result<(), IOError>` - result of operation;
+///
+/// # Example
+/// ```
+/// let mut nn = FeedForward::new(&[2, 2, 1]);
+/// /* train here your neural network */
+/// save(&nn, "test.flow");
+/// ```
 pub fn save<T: serde::Serialize>(obj: &T, file_path: &str) -> Result<(), IOError>{
     let mut file = File::create(file_path).map_err(IOError::IO)?;
     let encoded: Vec<u8> = serialize(obj, Infinite).map_err(IOError::Encoding)?;
@@ -26,7 +52,17 @@ pub fn save<T: serde::Serialize>(obj: &T, file_path: &str) -> Result<(), IOError
     Ok(())
 }
 
-
+/// Loads and restores the neural network from file.
+///
+/// * `file_path: &'b str` - path to the file;
+/// * `return -> Result<T, IOError>` - if Ok returns loaded neural network (Note, you must
+/// apparently specify the type T).
+///
+/// # Example
+/// ```
+/// let mut new_nn: FeedForward = load("test.flow")
+///     .unwrap_or(FeedForward::new(&[2, 2, 1]));
+/// ```
 pub fn load<'b, T>(file_path: &'b str) -> Result<T, IOError> where for<'de> T: serde::Deserialize<'de>{
     let mut content: Vec<u8> = Vec::new();
     let mut file = File::open(file_path).map_err(IOError::IO)?;
